dcf1b5734ebc47948fdba73a184eef5770713a98
unknown <kaushikyash262@gmail.com>
2023-08-27 09:49:05 +0530

    Components Layout completed

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+const App = () => (
+  <main className="relative">
+    Nav
+    <section className="xl:padding-1 wide:padding-b padding-r">
+      Hero
+    </section>
+    <section className="padding">
+      PopularProducts
+    </section>
+    <section className="padding">
+      SuperQuality
+    </section>
+    <section className="padding-x py-10">
+      Services
+    </section>
+    <section className="padding">
+      SpecialOffers
+    </section>
+    <section className="bg-pale-blue padding">
+      CustomerReviews
+    </section>
+    <section className="paddingx sm:py-32 py-16 w-full">
+      SpecialOffers
+    </section>
+    <section className="bg-black text-white padding-x padding-t pb-8">
+      SpecialOffers
+    </section>
+  </main>
+);
+
+export default App
